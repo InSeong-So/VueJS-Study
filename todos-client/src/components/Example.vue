4fe5547f7358e740db788afbfe09b18898673860
InSeong-So <goflvhxj2547@gmail.com>
2020-01-28 21:06:58 +0900

    vue study 용 토이프로젝트 - todo App 제작 시작

---
+++ todos-client/src/components/Example.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <div class="card-header">Panel heading without title</div>
+    <div class="card-body">
+      Panel content
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  data () {
+    return {
+      msg: 'Basic panel example'
+    }
+  }
+}
+</script>
