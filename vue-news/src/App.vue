cefd23bfea0d027aeb11439c5a8791d363dd52d7
sisparang <goflvhxj2547@gmail.com>
2021-03-21 23:48:53 +0900

    vue-router transition, 컴포넌트 스타일링

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -2,9 +2,12 @@
   <div id="app">
     <!-- Strongly Recommended-->
     <ToolBar></ToolBar>
+    <!-- name에 들어오는 내용이 css의 prefix가 됨 -->
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
     <!-- Essential -->
     <!-- <tool-bar></tool-bar> -->
-    <router-view></router-view>
   </div>
 </template>
 
@@ -23,5 +26,30 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+
+a.router-link-exact-active {
+  text-decoration: underline;
+}
+
+/* Router Transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+
+.page-enter,
+.page-leave-to {
+  opacity: 0;
 }
 </style>
