f090846cb38396ae86cf87170f3e1d94d9ccd73c
sisparang <goflvhxj2547@gmail.com>
2021-03-22 11:40:23 +0900

    공통 컴포넌트 구현

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <ul class="news-list">
+    <ListItem></ListItem>
+    <!-- <ul class="news-list">
       <li v-for="item in this.$store.state.news" class="post">
         <!-- 포인트 영역 -->
         <div class="points">
@@ -21,7 +22,7 @@
           </small>
         </div>
       </li>
-    </ul>
+    </ul> -->
 
     <!-- <p v-for="item in askItems">
       <router-link :to="`/item/${item.id}`">
@@ -33,59 +34,33 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import ListItem from "../components/ListItem.vue";
+
+// import { mapState, mapGetters } from "vuex";
 
 export default {
-  computed: {
-    // 1 : 번잡함
-    ask() {
-      return this.$store.state.ask;
-    },
-    // 2 : 별로 간편하지 않음(ECMA6 산개연산자)
-    ...mapState({
-      fetchedAsk: (state) => state.ask,
-    }),
-    // 3 : 변수에 저장하여 데이터를 사용하고 싶은 경우
-    ...mapGetters({
-      // 해당 컴포넌트에서 사용할 변수 : vuex의 getter가사용하는 이름
-      askItems: "fetchedAsk",
-    }),
-    // 3-1 : 데이터를 바로 사용하고 싶은 경우
-    ...mapGetters(["fetchedAsk"]),
+  components: {
+    ListItem,
   },
-  created() {
-    this.$store.dispatch("FETCH_ASK");
-  },
+  // computed: {
+  //   // 1 : 번잡함
+  //   ask() {
+  //     return this.$store.state.ask;
+  //   },
+  //   // 2 : 별로 간편하지 않음(ECMA6 산개연산자)
+  //   ...mapState({
+  //     fetchedAsk: (state) => state.ask,
+  //   }),
+  //   // 3 : 변수에 저장하여 데이터를 사용하고 싶은 경우
+  //   ...mapGetters({
+  //     // 해당 컴포넌트에서 사용할 변수 : vuex의 getter가사용하는 이름
+  //     askItems: "fetchedAsk",
+  //   }),
+  //   // 3-1 : 데이터를 바로 사용하고 싶은 경우
+  //   ...mapGetters(["fetchedAsk"]),
+  // },
+  // created() {
+  //   this.$store.dispatch("FETCH_ASK");
+  // },
 };
 </script>
-
-<style scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-
-.news-title {
-  margin: 0;
-}
-
-.link-text {
-  color: #828282;
-}
-</style>
