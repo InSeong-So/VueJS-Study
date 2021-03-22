105e1e6a1519b92e0130f7aec6dc5e29396cf723
sisparang <goflvhxj2547@gmail.com>
2021-03-22 18:34:50 +0900

    라우터 네비게이션 가드 사용

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -9,6 +9,9 @@ import ItemView from '../views/ItemView.vue'
 // + 같은 단어 동시 선택 단축키 : Ctrl + D, Cmd + D
 // function이므로 소문자 사용
 import createListView from '../views/CreateListView.js'
+// beforeEnter를 위함
+import bus from '../utils/bus.js'
+import { store } from '../store/index.js';
 
 Vue.use(VueRouter);
 
@@ -26,6 +29,35 @@ export const router = new VueRouter({
             // component : 특정 url 주소로 갔을 때 표시될 컴포넌트
             component: NewsView, // mixins 사용
             // component: createListView('NewsView'),
+            // 라우터 - beforeEnter = 기본 인자로 받음
+            beforeEnter: (to, from, next) => {
+                // to : 이동할 URL의 라우팅 정보
+                // console.log('to', to);
+                // from : 현재 URL의 라우팅 정보
+                // console.log('from', from);
+                // function next() 를 호출해야지만 to로 이동함
+                // console.log(next);
+                // 가장 자주 사용하는 방법
+                // if (to.matched === '') {
+                //     next();
+                // }
+                // else if (to.auth) {
+
+                // }
+                // else {
+                //     router.replace('/');
+                // }
+                bus.$emit("start:spinner");
+                store.dispatch("FETCH_LIST", to.name)
+                    .then(() => {
+                        console.log("fetched");
+                        bus.$emit("end:spinner");
+                        next();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
         },
         {
             path: '/ask',
