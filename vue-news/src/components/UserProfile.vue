2000f517cf440be7676c87c22586efc17985a286
sisparang <goflvhxj2547@gmail.com>
2021-03-22 12:28:45 +0900

    사용자 컴포넌트 데이터 흐름 처리 1

---
+++ vue-news/src/components/UserProfile.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="user-container">
+      <div>
+        <i class="fas fa-user"></i>
+      </div>
+      <div class="user-description">
+        <div>{{ userInfo.id }}</div>
+        <div class="time">
+          {{ userInfo.created }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // UserView.vue에서 store에 user 데이터를 담는다.
+  // 이미 store에 담겨있으므로 해당 컴포넌트에서 그대로 가져다 쓴다.
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
+    <UserProfile></UserProfile>
+    <!-- <p>name : {{ userInfo.id }}</p>
     <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <p>created : {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
+import UserProfile from "../components/UserProfile.vue";
+
 export default {
-  computed: {
-    // 태그 안에 코드들이 최대한 정돈되게 해야한다.
-    // 어느 위치에 어떤 변수가 있는지 모르는 상황에서는
-    // 최대한 정리해야한다.
-    userInfo() {
-      return this.$store.state.user;
-    },
+  components: {
+    UserProfile,
   },
   created() {
     const userName = this.$route.params.id;
-    // axios.get(`https://api.hnpwa.com/v0/user/${tuserName}.json`);
-    // payload, 여러 인자는 무조건 하나의 객체로 묶어서 보낼 것
     this.$store.dispatch("FETCH_USER", userName);
   },
 };
