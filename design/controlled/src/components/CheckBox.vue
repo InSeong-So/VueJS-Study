31b75bb302f9456596af25f25c7b7156d543a388
sisparang <goflvhxj2547@gmail.com>
2021-03-24 07:45:55 +0900

    컴포넌트 디자인 패턴

---
+++ design/controlled/src/components/CheckBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="checkbox" :value="value" @click="toggleCheckBox" />
+</template>
+
+<script>
+export default {
+  // @input 이벤트 : 올릴 때
+  // :value 값 : 내릴 때
+  props: ["value"],
+  methods: {
+    toggleCheckBox() {
+      this.$emit("input", !this.value);
+    },
+  },
+};
+</script>
