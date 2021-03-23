8f71e2dcbed78e85bf24440968b834ef00cba9d5
sisparang <goflvhxj2547@gmail.com>
2021-03-24 07:03:03 +0900

    차트 라이브러리 모듈화 기능별 결합

--- vue-charts/chart-lib/src/App.vue
+++ vue-charts/chart-lib/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h1>Chart.js</h1>
-    <BarChart></BarChart>
+    <BarChart
+      @refresh:chart="refreshChart"
+      :propsData="chartDataSet"
+    ></BarChart>
     <LineChart></LineChart>
   </div>
 </template>
@@ -15,6 +18,39 @@ export default {
     BarChart,
     LineChart,
   },
+  data() {
+    return {
+      chartDataSet: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  },
+  methods: {
+    refreshChart() {
+      this.chartDataSet.data = [9, 19, 29, 39, 49, 59];
+    },
+  },
+  created() {},
 };
 </script>
 
