3a46e50206e01f36e4306f545aa6c6003d577a5a
sisparang <goflvhxj2547@gmail.com>
2021-04-11 23:13:33 +0900

    학습 노트 데이터 조회 끝

--- vue-til/src/api/common/interceptors.js
+++ vue-til/src/api/common/interceptors.js
@@ -3,9 +3,6 @@ import store from '@/store/index';
 export function setInterceptors(instance) {
   instance.interceptors.request.use(
     function(config) {
-      // Do somthing before request is sent
-      //   console.log(config);
-      console.log(store.state.token);
       config.headers.Authorization = store.state.token;
       return config;
     },
--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,17 +1,10 @@
 import axios from 'axios';
 import { setInterceptors } from './common/interceptors';
 
-// instance setup
+// axios 초기화
 function createInstance() {
   const instance = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
-    // 토큰을 항상 Request.headers의 Authorization에 담아서 보낸다.
-    // 기본적인 API 통신 방법
-    // headers: {
-    // vuex로 header에 token 담기
-    // Authorization: store.state.token, : 옳지 못함, 이미 인스턴스가 create 된 상태이므로 authorization의 값을 변경시킬 수 없음
-    // axios interceptor로 적용이 가능하다.
-    // },
   });
 
   // 공통설정을 한 뒤 인터셉터로 처리
@@ -20,12 +13,19 @@ function createInstance() {
 
 const instance = createInstance();
 
+// 회원가입 API
 function registerUser(userData) {
   return instance.post('signup', userData);
 }
 
+// 로그인 API
 function loginUser(userData) {
   return instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+// 학습 노트 데이터를 조회하는 API
+function fetchPosts() {
+  return instance.get('posts');
+}
+
+export { registerUser, loginUser, fetchPosts };
