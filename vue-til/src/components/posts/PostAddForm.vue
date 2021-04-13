c4bcdc55b07dfac77c3ea2defe2b75176a24c1e1
sisparang <goflvhxj2547@gmail.com>
2021-04-13 22:03:31 +0900

    학습 노트 데이터 생성

---
+++ vue-til/src/components/posts/PostAddForm.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="contents">
+    <h1 class="page-header">
+      Create Posts
+    </h1>
+    <div class="form-wrapper">
+      <form class="form" @submit.prevent="submitForm">
+        <div>
+          <label for="title">Title: </label>
+          <input id="title" type="text" v-model="title" />
+        </div>
+        <div>
+          <label for="contents">Contents:</label>
+          <textarea id="contents" type="text" rows="5" v-model="contents" />
+          <p v-if="!isContentsValid" class="validation-text warning">
+            Contents length must be less than 200
+          </p>
+        </div>
+        <button type="submit" class="btn">Create</button>
+      </form>
+      <p class="log">
+        {{ logMessage }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { createPosts } from '@/api/index';
+
+export default {
+  data() {
+    return {
+      title: '',
+      contents: '',
+      logMessage: '',
+    };
+  },
+  computed: {
+    isContentsValid() {
+      return this.contents.length <= 200;
+    },
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const { data } = await createPosts({
+          title: this.title,
+          contents: this.contents,
+        });
+        console.log(data);
+      } catch (error) {
+        this.logMessage = error.response.data.message;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-wrapper .form {
+  width: 100%;
+}
+
+.btn {
+  color: white;
+}
+</style>
